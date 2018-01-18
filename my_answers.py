6fe3d5a444f8cf3784a42fc572b64d8769e18928
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2018-01-18 23:48:41 +0100

    WIP with no dropout and setup with droput for the next run

--- my_answers.py
+++ my_answers.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import Dense, Activation, LSTM
+from keras.regularizers import L1L2
 import keras
 
 
@@ -34,19 +34,31 @@ def build_part1_RNN(window_size):
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
-    punctuation = ['!', ',', '.', ':', ';', '?']
+    punctuation = [' ', '!', ',', '.', ':', ';', '?']
+    charset = ''.join(punctuation) + 'abcdefghijklmnopqrstuvwxyz'
+    # keep only valid characters, in one run we should clean the text
+    return ''.join([c for c in text if c in charset])
 
-    return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
-
-    return inputs,outputs
+    for i in range(0, len(text) - window_size, step_size):
+        # from i to i+windows+size (excluded)
+        inputs.append(text[i:i+window_size])
+        # only the character at index i+window_size
+        outputs.append(text[i+window_size])
+    return inputs, outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    # I've seen improvements in the generated text after adding some regularization and dropout,
+    # but I hadn't had the time to explore further
+    model.add(LSTM(200, input_shape=(window_size, num_chars), dropout=0.3, kernel_regularizer=L1L2(l1=0.1, l2=0.1)))
+    model.add(Dense(num_chars))
+    model.add(Activation("softmax"))
+    return model
